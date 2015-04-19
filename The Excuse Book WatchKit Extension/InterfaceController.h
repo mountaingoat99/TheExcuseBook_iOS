5599c67aafa654b9c2d588e1dda1cdd76df0f976
Jeremey Rodriguez <jeremey.rodriguez@outlook.com>
2015-04-19 09:22:04 -0500

    Added a WatchKit Project

---
+++ The Excuse Book WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  The Excuse Book WatchKit Extension
+//
+//  Created by Jeremey Rodriguez on 4/19/15.
+//  Copyright (c) 2015 SingleCog Software. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
