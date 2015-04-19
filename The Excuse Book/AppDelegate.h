656253d8cac18cb0430a94d3e6fb9dbbc21fc5be
Jeremey Rodriguez <jeremey.rodriguez@outlook.com>
2015-04-19 09:13:31 -0500

    Initial Commit

---
+++ The Excuse Book/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  The Excuse Book
+//
+//  Created by Jeremey Rodriguez on 4/19/15.
+//  Copyright (c) 2015 SingleCog Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ The Excuse Book/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  The Excuse Book
+//
+//  Created by Jeremey Rodriguez on 4/19/15.
+//  Copyright (c) 2015 SingleCog Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
