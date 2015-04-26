e66e975ebb0161b3a01bf9766c81866935916646
Jeremey Rodriguez <jeremey.rodriguez@outlook.com>
2015-04-25 21:37:38 -0500

    Default controller is working

--- The Excuse Book/DefaultController.h
+++ The Excuse Book/DefaultController.h
@@ -10,4 +10,13 @@
 
 @interface DefaultController : NSObject
 
+-(NSString*)DefaultSportID;
+
+-(NSString*)DefaultSportName:(NSString*)sportID;
+
+-(BOOL)AddNewSport:(NSString*)sportName;
+
+// android version has a method to add sport name and id at once
+// but that is for the wear app, shouldn't need it for the iOS version
+
 @end
--- The Excuse Book/DefaultController.m
+++ The Excuse Book/DefaultController.m
@@ -7,7 +7,36 @@
 //
 
 #import "DefaultController.h"
+#import "SportsModel.h"
+#import "DefaultModel.h"
 
 @implementation DefaultController
+
+-(NSString*)DefaultSportID {
+    
+    DefaultModel *defaultSport = [[DefaultModel alloc] init];
+    
+    return [defaultSport CheckDefaultID];
+    
+}
+
+-(NSString*)DefaultSportName:(NSString*)sportID {
+    
+    SportsModel *sport = [[SportsModel alloc] init];
+    
+    return [sport GetOneSportName:sportID];
+    
+}
+
+-(BOOL)AddNewSport:(NSString*)sportName {
+    
+    SportsModel *sport = [[SportsModel alloc] init];
+    
+    return [sport AddNewSport:sportName];
+    
+}
+
+// android version has a method to add sport name and id at once
+// but that is for the wear app, shouldn't need it for the iOS version
 
 @end
